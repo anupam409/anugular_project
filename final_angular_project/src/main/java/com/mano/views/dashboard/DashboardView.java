796e6195a6d759f665f03a08d639339777b2b852
anupam409 <anupamkumar409@gmail.com>
2020-08-19 21:24:58 +0530

    Anugular JS final Source code

---
+++ final_angular_project/src/main/java/com/mano/views/dashboard/DashboardView.java
@@ -0,0 +1,22 @@
+package com.mano.views.dashboard;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.mano.views.main.MainView;
+
+@Route(value = "dashboard", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+@PageTitle("Dashboard")
+@CssImport("styles/views/dashboard/dashboard-view.css")
+public class DashboardView extends Div {
+
+    public DashboardView() {
+        setId("dashboard-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
